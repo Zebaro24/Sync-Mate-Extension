dc5fd40faf225a6843c0cd0d24918f7523f544c7
Zebaro <denok100100@gmail.com>
2025-09-10 17:22:50 +0200

    Add formatTime util

--- src/shared/utils/time.js
+++ src/shared/utils/time.js
@@ -1,3 +1,10 @@
 export function roundTime(t) {
     return Math.round(Number(t || 0) * 1000) / 1000;
 }
+
+export function formatTime(seconds) {
+    const mins = Math.floor(seconds / 60);
+    const secs = Math.floor(seconds % 60);
+    const paddedSecs = secs.toString().padStart(2, '0');
+    return `${mins}:${paddedSecs}`;
+}
