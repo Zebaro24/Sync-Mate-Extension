e6cfb3f0cb154cc494f1b9bd70e858abc6e79801
Zebaro <denok100100@gmail.com>
2026-06-27 13:22:39 +0200

    Make getItem generic

--- src/shared/storage.ts
+++ src/shared/storage.ts
@@ -1,8 +1,8 @@
 import { browser } from "#imports";
 
-export async function getItem(key: string) {
+export async function getItem<T = string>(key: string): Promise<T | undefined> {
     const data = await browser["storage"].local.get(key);
-    return data?.[key];
+    return data?.[key] as T | undefined;
 }
 
 export async function setItem(key: string, value: any) {
